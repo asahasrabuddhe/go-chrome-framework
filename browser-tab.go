eaf292198c7b8ebeddf805620a2bcb09c9673163
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2020-01-01 12:01:29 +0530

    add option struct to override screenshot size

--- browser-tab.go
+++ browser-tab.go
@@ -10,11 +10,20 @@ import (
 type BrowserTab interface {
 	Navigate(url string, timeout time.Duration) (bool, error)
 	GetHTML(timeout time.Duration) (string, error)
-	CaptureScreenshot(timeout time.Duration) (string, error)
+	CaptureScreenshot(opts ScreenshotOpts, timeout time.Duration) (string, error)
 	Exec(javascript string, timeout time.Duration) (*runtime.EvaluateReply, error)
 	GetClient() *cdp.Client
 	GetTargetID() target.ID
 	AttachHook(hook ClientHook)
 }
 
+type ClientHook func(c *cdp.Client) error
 
+type ClientHooks []ClientHook
+
+type ScreenshotOpts struct {
+	Width             int
+	Height            int
+	DeviceScaleFactor float64
+	Mobile            bool
+}
